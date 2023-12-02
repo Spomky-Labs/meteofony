cd76f2232d5ff7364b07603a06cba077eca98503
Florent Morselli <florent.morselli@spomky-labs.com>
2023-12-02 11:44:10 +0100

    Fix missing CSS import

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,4 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './styles/app.css'
