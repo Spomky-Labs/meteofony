ca44f84ba2d70c7221ba93cb627e5378f96e688d
Florent Morselli <florent.morselli@spomky-labs.com>
2023-12-05 12:11:26 +0100

    Add "Remember Me" functionality to the login system
    Added a "Remember Me" checkbox to the login form that persists user sessions for a duration of one week. This was implemented by adding a 'remember_me' field in security.yaml, adding the RememberMeBadge in the UsernamePasswordAuthenticator, and creating a corresponding checkbox in the login.ui.twig form. This change enhances the user experience, reducing the frequency of having to login for regular users.

--- src/Security/UsernamePasswordAuthenticator.php
+++ src/Security/UsernamePasswordAuthenticator.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -39,6 +40,7 @@ final class UsernamePasswordAuthenticator extends AbstractLoginFormAuthenticator
         return new Passport(
             new UserBadge($username),
             new PasswordCredentials($request->request->get('password', '')),
+            new RememberMeBadge(),
             [new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token'))]
         );
     }
